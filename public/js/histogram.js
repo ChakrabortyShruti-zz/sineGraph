23b0f2c60a95514d7110576510fc1e2866395bb2
Shruti Chakraborty <shruti.chakraborty16@gmail.com>
2016-11-28 19:18:38 +0530

    made histogram for random set of data

--- public/js/histogram.js
+++ public/js/histogram.js
@@ -1,4 +1,4 @@
-var data = [0,0,0,0,0,0,0,1,2,2,2,3,3,4,5,5,5,5,5,5,6,6,7,8,9,10,11];
+var data = [0,0,1,1,2,2,3,4,5,5,8,7,5,7,8,9,10];
 
 const MARGIN = 30;
 const HEIGHT = 600;
@@ -6,36 +6,47 @@ const WIDTH = 600;
 const INNER_WIDTH = WIDTH - 2*MARGIN;
 const INNER_HEIGHT = HEIGHT - 2*MARGIN;
 
-var _xScale,_yScale;
 
 var  loadChart = function(){
-	d3.select('body')
+	console.log(data);
+	var svg = d3.select('body')
 		.append('svg')
 		.attr('width',WIDTH)
 		.attr('height',HEIGHT);
 
-	_xScale = d3.scaleLinear().domain([0,11]).range([0,INNER_WIDTH]);
-	_yScale = d3.scaleLinear().domain([0,10]).range([INNER_HEIGHT,0]);
+	var g = svg.append('g').attr('transform', 'translate(' + MARGIN + ',0)');
 
-	_svg = d3.select('svg')
-		.append('g')
-		.classed('graphs',true)
-		.attr('transform', 'translate('+MARGIN+', '+MARGIN+')');
+	var x = d3.scaleLinear()
+		.domain([0,10])
+	    .rangeRound([0, INNER_WIDTH]);
+
+	var xAxis = d3.axisBottom(x);
+	svg.append('g')
+	    .attr('class', 'xAxis')
+	    .attr('transform', 'translate('+MARGIN+',' + INNER_HEIGHT + ')')
+	    .call(xAxis);
 
 	var histogram = d3.histogram()
-	    .domain(_xScale.domain());
+	    .domain(x.domain())
+	    .thresholds(x.ticks(10))
+	    
+	var bins = histogram(data);
+	console.log(bins);
 
-	var bin = histogram(data);
-	console.log(bin);
-	var bar =_svg.selectAll("rect")
-		.data(bin)
-		.enter()
-		.append("rect")
-		.attr('x',function(d,i){return _xScale(i)})
-		.attr('y',function(d){return _yScale(d.length)})
-		.attr('width',function(d){return ((_xScale(d.x1) - _xScale(d.x0))-1)})
-		.attr('height',function(d){return INNER_HEIGHT - _yScale(d.length)})
-		.style('fill','grey');
+	var y = d3.scaleLinear()
+	    .domain([0, d3.max(bins, function(d) { return d.length; })])
+	    .range([INNER_HEIGHT, 0]);
+
+	var bar = g.selectAll('.bar')
+	  	.data(bins)
+	  	.enter()
+		.append('rect')
+	  	.attr('class', 'bar')
+	    .attr('x', function(d){return x(d.x0)})
+	    .attr('y', function(d){return y(d.length)})
+	    .attr('width', x(bins[0].x1) - x(bins[0].x0) - 1)
+	    .attr('height', function(d) { return INNER_HEIGHT - y(d.length); });
+
 }
 
 
